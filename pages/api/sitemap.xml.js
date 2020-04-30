37a74cce4b1792259b0bac2697d2347408fed0d1
Nguyen Duc Manh <manh@hihu.dev>
2020-04-30 19:34:43 +0000

    sitemap

---
+++ pages/api/sitemap.xml.js
@@ -0,0 +1,55 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { SitemapStream, streamToPromise, EnumChangefreq } from "sitemap";
+import { createGzip } from "zlib";
+import { getBlogEntries } from "../../src/services/contentful";
+import moment from "moment";
+const getServerSideProps = async () => {
+  const data = await getBlogEntries();
+
+  return JSON.parse(JSON.stringify(data));
+};
+
+export default async (req, res) => {
+  if (!res) return {};
+  try {
+    // Set response header
+    res.setHeader("content-type", "application/xml");
+    res.setHeader("Content-Encoding", "gzip");
+
+    // A Transform for turning a Readable stream of either SitemapItemOptions or url strings into a Sitemap.
+    // The readable stream it transforms must be in object mode.
+    const smStream = new SitemapStream({
+      hostname: "https://reactard.xyz",
+    });
+
+    const pipeline = smStream.pipe(createGzip());
+    // Add any static entries here
+    smStream.write({
+      url: "/",
+      changefreq: EnumChangefreq.WEEKLY,
+    });
+
+    // E.g. we create a sitemap.xml for articles
+    // Set articles change frequencey is weekly
+    const articles = await getServerSideProps();
+    articles.map((post) => {
+      console.log(post);
+      smStream.write({
+        url: `/post/${post.slug}`,
+        changefreq: EnumChangefreq.WEEKLY,
+      });
+    });
+    smStream.end();
+
+    // cache the response
+    // streamToPromise.then(sm => sitemap = sm)
+    streamToPromise(pipeline);
+    // stream the response
+    pipeline.pipe(res).on("error", (e) => {
+      throw e;
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).end();
+  }
+};
