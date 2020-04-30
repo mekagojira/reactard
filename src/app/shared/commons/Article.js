b0099364fb57f665370c11758c4915c193244d71
Nguyen Duc Manh <manh@hihu.dev>
2020-04-30 13:24:06 +0000

    Code syntax

--- src/app/shared/commons/Article.js
+++ src/app/shared/commons/Article.js
@@ -1,11 +1,35 @@
 import PropTypes from "prop-types";
 import ReactMarkdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import {
+  gruvboxDark,
+  agate,
+} from "react-syntax-highlighter/dist/cjs/styles/hljs";
 
+const CodeRendered = (props) => {
+  return (
+    <SyntaxHighlighter language={props.language || "javascript"} style={agate}>
+      {props.value || props.children}
+    </SyntaxHighlighter>
+  );
+};
 export default function Article(props) {
   const { source } = props;
   return (
     <div className="article">
-      {source ? <ReactMarkdown source={source} /> : props.children}
+      {source ? (
+        <ReactMarkdown
+          source={source}
+          renderers={{
+            Code: CodeRendered,
+            InlineCode: CodeRendered,
+            code: CodeRendered,
+            inlineCode: CodeRendered,
+          }}
+        />
+      ) : (
+        props.children
+      )}
     </div>
   );
 }
