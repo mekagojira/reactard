0bb5fdb8fe60bcbad7e6dde16ab78da3d17958ae
Manh Nguyen Duc <manh@ngducmanh.com>
2020-10-16 22:13:58 +0700

    Update

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
-require("dotenv").config({
-  path: process.env.NODE_ENV === "production" ? ".env" : ".env.local",
+require('dotenv').config({
+    path: process.env.NODE_ENV === 'production' ? '.env' : '.env.local',
 });
 
 const nextConfig = {
-  env: {
-    CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
-    CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
-  },
+    env: {
+        CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
+        CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
+    },
 };
 
 module.exports = nextConfig;
--- src/app/home-page/components/Post.js
+++ src/app/home-page/components/Post.js
@@ -1,46 +1,43 @@
-import PropTypes from "prop-types";
-import Card from "../../shared/commons/Card";
-import Link from "next/link";
-import Badge from "../../shared/commons/Badge";
+import PropTypes from 'prop-types';
+import Card from '../../shared/commons/Card';
+import Link from 'next/link';
+import Badge from '../../shared/commons/Badge';
 
 export default function Post(props) {
-  const { post } = props;
+    const { post } = props;
 
-  const renderTags = () => {
-    if (!post.tags) return null;
+    const renderTags = () => {
+        if (!post.tags) return null;
 
-    return post.tags.map((tag, i) => {
-      return (
-        <div className="mr-2" key={i}>
-          <Badge>{tag}</Badge>
-        </div>
-      );
-    });
-  };
+        return post.tags.map((tag, i) => {
+            return (
+                <div className="mr-2" key={i}>
+                    <Badge>{tag}</Badge>
+                </div>
+            );
+        });
+    };
 
-  const tags = renderTags();
+    const tags = renderTags();
 
-  return (
-    <Link href={`/post/${post.slug}`}>
-      <a>
-        <Card border="border-l-4 border-app">
-          <h2 className="font-medium text-gray-900 text-xl mb-2">
-            {post.title}
-          </h2>
-          <div className="pt-4">
-            <div className="flex flex-wrap">
-              {tags}
-              <span className="text-sm text-gray-700">
-                <span className="font-bold">{post?.publishedDate}</span>
-              </span>
-            </div>
-          </div>
-        </Card>
-      </a>
-    </Link>
-  );
+    return (
+        <Link href={`/post/${post.slug}`}>
+            <a>
+                <Card border="border-l-4 border-app">
+                    <h2 className="font-medium text-gray-900 text-xl mb-2">
+                        {post.title}
+                    </h2>
+                    <div className="pt-4">
+                        <div className="flex flex-wrap">
+                            {tags}
+                        </div>
+                    </div>
+                </Card>
+            </a>
+        </Link>
+    );
 }
 
 Post.propTypes = {
-  post: PropTypes.object.isRequired,
+    post: PropTypes.object.isRequired,
 };
